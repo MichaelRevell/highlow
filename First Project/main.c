bd563fc1745cef50ee2a434c1330074fc422e22f
Michael Revell <mikearevell@gmail.com>
2013-10-02 23:50:05 -0700

    High Low

--- First Project/main.c
+++ First Project/main.c
@@ -7,12 +7,29 @@
 //
 
 #include <stdio.h>
+#import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    @autoreleasepool {
+        int guess = 0;
+        int turn = 0;
+        int answer = arc4random() % 100 + 1;
+        while( guess != answer) {
+            NSLog(@"Enter a number between 1 and 100");
+            scanf("%i", &guess);
+            NSLog(@"You entered: %i", guess);
+            if (guess > answer) {
+                NSLog(@"You guessed too high!");
+            } else if (guess < answer) {
+                NSLog(@"You guessed too low!");
+            } else {
+                NSLog(@"You win");
+                break;
+            }
+            turn++;
+        }
+    }
     return 0;
 }
 
